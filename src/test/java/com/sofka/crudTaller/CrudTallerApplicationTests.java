4709572787a57cf4346c0993a594fe09b7142255
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-06-08 13:50:34 -0500

    Estructura del proyecto

---
+++ src/test/java/com/sofka/crudTaller/CrudTallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.crudTaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
